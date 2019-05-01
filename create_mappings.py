6f0960b9e8725e0a87de6672c430f6e5283b217a
Mihaela Stoycheva <mihaela.stoycheva@gmail.com>
2019-05-01 12:39:48 +0200

    Added a script that creates all the indexes witb their mappings; Insert index for repository and file

---
+++ create_mappings.py
@@ -0,0 +1,238 @@
+import os
+import base64
+import re
+import logging
+import config
+from elasticsearch import Elasticsearch
+
+repository_properties = {
+    "doc": {
+        "properties": {
+            "id": {
+                "type": "long"
+            },
+            "owner": {
+                "type": "keyword"
+            },
+            "created_at": {
+                "type": "date"
+            },
+            "description": {
+                "type": "text"
+            },
+            "name": {
+                "type": "keyword"
+            },
+            "html_url": {
+                "type": "keyword"
+            },
+            "open_issues_count": {
+                "type": "integer"
+            },
+            "pushed_at": {
+                "type": "date"
+            },
+            "stargazers_count": {
+                "type": "integer"
+            }
+        }
+    }
+}
+file_properties = {
+    "doc": {
+        "properties": {
+            "id": {
+                "type": "long"
+            },
+            "url": {
+                "type": "keyword",
+            },
+            "file_name": {
+                "type": "keyword"
+            },
+            "words": {
+                "type": "text"
+            }
+        }
+    }
+}
+interface_properties = {
+    "doc": {
+        "properties": {
+            "url": {
+                "type": "keyword"
+            },
+            "interface_name": {
+                "type": "keyword"
+            },
+            "interface_modifier": {
+                "type": "keyword"
+            },
+            "implements_interfaces": {
+                "type": "keyword"
+            }
+        }
+    }
+}
+class_properties = {
+    "doc": {
+        "properties": {
+            "url": {
+                "type": "keyword"
+            },
+            "class_name": {
+                "type": "keyword"
+            },
+            "access_modifier": {
+                "type": "keyword"
+            },
+            "is_abstract": {
+                "type": "boolean"
+            },
+            "is_static": {
+                "type": "boolean"
+            },
+            "is_final": {
+                "type": "boolean"
+            },
+            "annotation": {
+                "type": "text"
+            },
+            "extends_class": {
+                "type": "keyword"
+            },
+            "implements_interfaces": {
+                "type": "keyword"
+            }
+        }
+    }
+}
+method_properties = {
+    "doc": {
+        "properties": {
+            "url": {
+                "type": "keyword"
+            },
+            "method_name": {
+                "type": "keyword"
+            },
+            "access_modifier": {
+                "type": "keyword"
+            },
+            "is_abstract": {
+                "type": "boolean"
+            },
+            "is_static": {
+                "type": "boolean"
+            },
+            "is_final": {
+                "type": "boolean"
+            },
+            "return_type": {
+                "type": "keyword"
+            },
+            "input_type": {
+                "type": "keyword"
+            },
+            "cyclomatic_complexity": {
+                "type": "integer"
+            },
+            "token_count": {
+                "type": "integer"
+            },
+            "parameter_count": {
+                "type": "integer"
+            }
+        }
+    }
+}
+variable_proprties = {
+    "doc": {
+        "properties": {
+            "url": {
+                "type": "keyword"
+            },
+            "variable_name": {
+                "type": "keyword"
+            },
+            "variable_type": {
+                "type": "keyword"
+            },
+            "access_modifier": {
+                "type": "keyword"
+            },
+            "is_static": {
+                "type": "boolean"
+            },
+            "is_final": {
+                "type": "boolean"
+            }
+        }
+    }
+}
+package_properties = {
+    "doc": {
+        "properties": {
+            "url": {
+                "type": "keyword"
+            },
+            "package_name_kw": {
+                "type": "keyword"
+            },
+            "package_name_txt": {
+                "type": "text"
+            }
+        }
+    }
+}
+
+indices = [(config.repository_index, repository_properties), \
+(config.file_index, file_properties), (config.interface_index, interface_properties), \
+(config.class_index, class_properties), (config.method_index, method_properties),  \
+(config.variable_index, variable_proprties), (config.package_index, package_properties)]
+
+def create_index(es_object, index_name, properties):
+    created = False
+    settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": properties
+    }
+    try:
+        if not es_object.indices.exists(index_name):
+            # Ignore 400 means to ignore "Index Already Exist" error.
+            res = es_object.indices.create(index=index_name, ignore=400, body=settings)
+            if res['acknowledged']:
+                print('Created index', index_name)
+            else:
+                print('Index couldn\'t be created', index_name, res)
+        created = True
+    except Exception as ex:
+        print('Exception!', str(ex))
+    finally:
+        return created
+
+def delete_index(es_object, index_name):
+    if es_object.indices.exists(index_name):
+        res = es_object.indices.delete(index=index_name)
+
+
+logging.basicConfig(level=logging.INFO)
+bonsai = config.bonsai_url
+auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+es_header = [{
+ 'host': host,
+ 'port': 443,
+ 'use_ssl': True,
+ 'http_auth': (auth[0],auth[1])
+}]
+
+es = Elasticsearch(es_header)
+
+for index in indices:
+    delete_index(es, index[0])
+    create_index(es, index[0], index[1])
--- get_files.py
+++ get_files.py
@@ -3,6 +3,9 @@ from time import sleep
 import json
 import os
 import config
+import lizard
+import os, base64, re, logging
+from elasticsearch import Elasticsearch
 
 lang = 'Java'
 access_token = config.access_token;
@@ -11,7 +14,26 @@ sha = '/master?recursive=1&access_token={}'.format(access_token);
 per_page = 100
 requests = 0
 
-def create_save_file(repo_name, content_path, path, requests):
+# Log transport details (optional):
+logging.basicConfig(level=logging.INFO)
+
+# Parse the auth and host from env:
+bonsai = config.bonsai_url
+auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+# Connect to cluster over SSL using auth for best security:
+es_header = [{
+ 'host': host,
+ 'port': 443,
+ 'use_ssl': True,
+ 'http_auth': (auth[0],auth[1])
+}]
+
+# Instantiate the new Elasticsearch connection:
+es = Elasticsearch(es_header)
+
+def create_save_file(repo_name, content_path, path, requests, id, url):
     if requests == 5000:
         sleep(60*60)
         requests = 0
@@ -25,6 +47,13 @@ def create_save_file(repo_name, content_path, path, requests):
         response = urlopen(request).read().decode('utf-8')
         requests += 1
     print('Request for {}, requests {}'.format(path, requests))
+    doc = {
+        'id': id,
+        'url': url,
+        'file_name': os.path.basename(path),
+        'words': response
+    }
+    res = es.index(index=config.file_index, doc_type='doc', body=doc)
     file_path = './repos/{}/{}'.format(repo_name, path)
     if not os.path.exists(os.path.dirname(file_path)):
         os.makedirs(os.path.dirname(file_path))
@@ -32,8 +61,10 @@ def create_save_file(repo_name, content_path, path, requests):
     with open(file_path, 'a', encoding='utf-8') as file:
         file.write(response)
         file.close()
+    i = lizard.analyze_file(file_path)
+    for function_index in range(len(i.__dict__['function_list'])):
+        print(i.function_list[function_index].__dict__)
     return requests
-
 
 def download_page(page, requests):
     format_string = 'https://api.github.com/search/repositories?q=language:{}&sort=stars&order=desc&page={}&per_page={}&access_token={}'
@@ -43,6 +74,18 @@ def download_page(page, requests):
 
     repositories = json.loads(repositories)
     for repo in repositories['items']:
+        doc = {
+            'id': repo['id'],
+            'owner': repo['owner']['login'],
+            'created_at': repo['created_at'],
+            'description': repo['description'],
+            'name': repo['name'],
+            'html_url': repo['html_url'],
+            'open_issues_count': repo['open_issues_count'],
+            'pushed_at': repo['pushed_at'],
+            'stargazers_count': repo['stargazers_count']
+        }
+        res = es.index(index=config.repository_index, doc_type='doc', body=doc)
         content_path = repo['contents_url'].replace('{+path}', '')
         if requests == 5000:
             sleep(60*60)
@@ -55,7 +98,7 @@ def download_page(page, requests):
         files = json.loads(files)
         for file in files['tree']:
             if file['path'].endswith('.java'):
-                requests = create_save_file(repo['name'], content_path, file['path'], requests)
+                requests = create_save_file(repo['name'], content_path, file['path'], requests, repo['id'], file['url'])
 
 for i in range(10, 15):
     print('\n\nDownloading page {}'.format(i))
